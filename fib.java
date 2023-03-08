d87d3a4be5e2a72bd06bb6750dcfc78075b2416b
LuisJ1705 <112445175+LuisJ1705@users.noreply.github.com>
2023-03-07 22:02:27 -0500

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class fib
+{
+    //method to return the n-th digit from the fibonacci sequence
+    public static int fibonacci(int n)
+    {
+    //if the n input is 1 or less then return n
+    if (n <= 1)
+       return n;
+    //recursive fibonacci sequence functions
+    return fibonacci(n-1) + fibonacci(n-2);
+    }
+    //main method 
+    public static void main (String args[])
+    {
+    //declare n variable
+    int n;
+    //create Scanner class input
+    Scanner input=new Scanner(System.in);
+    //get input n from user
+    System.out.println("Enter any value for n: ");
+    n=input.nextInt();
+    //print n-th number in sequence
+    System.out.println("The "+n+"th value of the Fibonacci sequence is "+fibonacci(n));
+    }
+}
